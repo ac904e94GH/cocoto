5ccc7283fd1960c768522d94692088800aa146ca
ac904e94GH <ac904e94@gmail.com>
2017-04-18 21:32:42 +0900

    First Upload

---
+++ CocotoGit/CocotoGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocotoGit
+//
+//  Created by 김성년 on 2017. 4. 18..
+//  Copyright © 2017년 김성년. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
