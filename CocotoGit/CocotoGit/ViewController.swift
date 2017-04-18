9c0b4f9c00dacfb6b64c7f1f0632a9fc6d7d3a96
김성년 <ac904e94@Kim-ui-MacBook-Pro.local>
2017-04-18 22:18:29 +0900

    Add TextField

--- CocotoGit/CocotoGit/ViewController.swift
+++ CocotoGit/CocotoGit/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var saveBtn: UIButton!
     
+    @IBOutlet wear var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
